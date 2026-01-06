2703b2b2fe7f8b6eedcb3dc4065e41a0780c3309
jl <joanna.laskowicz@gmail.com>
2026-01-07 00:09:35 +0100

    commit3nr3

--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -1,3 +1,6 @@
-
-
-class GraphBuilder
+from langgraph.graph import StateGraph
+from src.langgraphagenticai.state.state import State
+class GraphBuilder:
+    def __init__(self,model):
+        self.llm=model
+        self.graph_builder= StateGraph(State)
