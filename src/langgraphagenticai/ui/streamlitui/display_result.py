fefb8fd70dc976383d1759028f5f655da099ff84
jl <joanna.laskowicz@gmail.com>
2026-01-10 21:00:13 +0100

    m

--- src/langgraphagenticai/ui/streamlitui/display_result.py
+++ src/langgraphagenticai/ui/streamlitui/display_result.py
@@ -3,4 +3,7 @@ from langchain_core.messages import HumanMessage,AIMessage,ToolMessage
 import json
 
 class DisplayResultStreamlit:
-    def __init__(self):
+    def __init__(self, usecase, graph, user_message):
+        self.usecase = usecase
+        self.graph= graph
+        self.user_message=user_message
